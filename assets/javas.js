daa669b687b0bb5eacbf831a19417abd43ec2ed0
RyanTheFerreira <108907295+RyanTheFerreira@users.noreply.github.com>
2023-05-02 19:18:49 -0400

    added submit handler and city lookup

--- assets/javas.js
+++ assets/javas.js
@@ -1,10 +1,36 @@
-var now = dayjs();
-var currentDate = (now.format("MM/DD/YYYY"));
-var city ="";
-var citySearch = $("#citySearch");
-var citySearchButton = $("#citySearchBtn");
+const apiKey = "356a95c45b702fb828c2ea53d5161e34";
+var searchButton = document.querySelector("#submit-form");
+var nameInputEl = document.querySelector("#city-name");
+const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || []
+const maxTableSize = 10;
+const cityNamePattern = /^(.+?)(?:, ([a-zA-Z]{2}), ([a-zA-Z]{2}))?$/;
 
+function formSubmitHandler(event) {
+    event.preventDefault();
+    var value = nameInputEl.value.trim();
 
-function deleteItems(){
-    localStorage.clear();
+    if (cityNamePattern.test(value)){
+        var city = value;
+        cityLookup(city);
+    } else {
+        alert("Invalid input")
+    }
 }
+
+
+function cityLookup(city) {
+    var cityName = city;
+    var limit = 1;
+    
+    const cityIndex = searchHistory.indexof(city);
+    if (cityIndex > -1) {
+        searchHistory.splice(cityIndex, 1);
+    }
+}
+
+if (searchHistory.length >= maxTableSize ) {
+    searchHistory.pop();
+
+}
+
+
